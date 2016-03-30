2507deed8cb269607ceac03c9fa72bf604d278f1
Liam Heisler <liamh2017@gmail.com>
2016-03-30 09:28:28 -0400

    Update VacuumRobot.c

--- VacuumRobot.c
+++ VacuumRobot.c
@@ -1,49 +1,91 @@
-
 /*
   Blank Simple Project.c
   http://learn.parallax.com/propeller-c-tutorials 
 */
 #include "simpletools.h"                      // Include simple tools
-#include "servodiffdrive.h" 
 #include "colorpal.h"
-#include "ping.h"
-
-#define leftMotor 16
-#define rightMotor 17
-#define leftBump 1
-#define rightBump 15
-#define ultrasonic 3
-#define redLight 14
+#include "ping.h"  
+#include "servodiffdrive.h" 
 
 colorPal *cpal;                              
 int cpSIO = 0;  //Find exact number, check pins                            
 int r, g, b;
 
-void drive();
-void checkCPal(); 
-void followPattern(); 
-
-int main()    
-{  
-  cpal = colorPal_open(cpSIO);
-  drive_pins(leftMotor, rightMotor);  
-  
+int main()                                    // Main function
+{
+  cpal = colorPal_open(cpSIO); 
+  drive_pins(16, 17); 
+   
   while(1)
-  {
-    colorPal_getRGB(cpal, &r, &g, &b);
-    drive();
-  }
+  {    
+    colorPal_getRGB(cpal, &r, &g, &b);  
+    
+    print("r = %04d g = %04d b = %04d\n", r, g, b); 
+    
+    //blue 
+    checkInput(); 
+    
+    if((r > 12) && (r < 20) && (g > 9) && (g < 16) && (b > 30) && (b < 37))  
+    {
+      print("Blue!\n"); 
+      drive_sleep(); 
+      for(int i = 0; i < 5; i++) 
+      {
+        high(14); 
+        pause(500);
+        low(14); 
+        pause(500);   
+      }
+      drive_speeds(90, 45); 
+      pause(1000);     
+      //return 1;       
+    }
+    //red
+    if((r > 41) && (r < 50) && (g > 1) && (g < 7) && (b > 0) && (b < 7))  
+    {     
+      print("Red!\n");
+      drive_sleep();  
+      for(int i = 0; i < 5; i++) 
+      {
+        high(14); 
+        pause(500);
+        low(14); 
+        pause(500);   
+      }
+      drive_speeds(90, 45);
+      pause(1000); 
+      //return 1; 
+    }
+    //orange
+    if((r > 60) && (r < 75) && (g > 20) && (g < 40) && (b > 25) && (b < 37))  
+    {
+      print("Orange!\n"); 
+      drive_sleep(); 
+      for(int i = 0; i < 5; i++) 
+      {
+        high(14); 
+        pause(500);
+        low(14); 
+        pause(500);   
+      }  
+      drive_speeds(90, 45);
+      pause(1000); 
+      //return 1;     
+    }
+    else
+    {
+      followPattern(); 
+    } 
+    pause(100); 
+  }  
 }
-
-
-void drive() 
-{  
-  int leftBumper = input(leftBump);
-  int rightBumper = input(rightBump); 
-  int distance = input(ultrasonic);
+void checkInput() 
+{
+  int leftBump = input(1); 
+  int rightBump = input(15);
+  int distance = ping_cm(3); 
   
-    print("left = %d right = %d; distance = %d\n", leftBumper, rightBumper, distance);  
-    if(leftBumper == 1) 
+    if(leftBump == 1) 
     {
       print("leftBump pressed, turning right\n");
       drive_speeds(-25, -60); //reverse
@@ -51,7 +93,7 @@ void drive()
       drive_speeds(100, -10); //turn right
       pause(400);   
     }
-    else if(rightBumper == 1)
+    else if(rightBump == 1)
     {
       print("rightBump pressed, turning left\n"); 
       drive_speeds(-25, -60); //reverse
@@ -65,100 +107,10 @@ void drive()
       pause(1200); 
       drive_speeds(-5, 100); 
       pause(700); 
-    }
-    //blue
-    if((r > 12) && (r < 20) && (g > 9) && (g < 16) && (b > 30) && (b < 37))  
-    {
-      print("Blue!\n"); 
-      for(int i = 0; i < 5; i++) 
-      {
-        high(14); 
-        pause(500);
-        low(14); 
-        pause(500);   
-      }    
-      //return 1;       
-    }
-    //red
-    if((r > 41) && (r < 50) && (g > 1) && (g < 7) && (b > 0) && (b < 7))  
-    {     
-      print("Red!\n"); 
-      for(int i = 0; i < 5; i++) 
-      {
-        high(14); 
-        pause(500);
-        low(14); 
-        pause(500);   
-      }
-      //return 1; 
-    }
-    //orange
-    if((r > 60) && (r < 75) && (g > 20) && (g < 40) && (b > 25) && (b < 37))  
-    {
-      print("Orange!\n"); 
-      for(int i = 0; i < 5; i++) 
-      {
-        high(14); 
-        pause(500);
-        low(14); 
-        pause(500);   
-      }  
-      //return 1;     
-    }
-    else 
-    {
-      followPattern();
-    }
-}
-void checkCPal() 
-{
-    //blue
-
+    } 
 }
 void followPattern() 
 {
-  int straightSpeed_Left = 90; 
-  int straightSpeed_Right = 40;  
-    if((r > 12) && (r < 20) && (g > 9) && (g < 16) && (b > 30) && (b < 37))  
-    {
-      print("Blue!\n"); 
-      for(int i = 0; i < 5; i++) 
-      {
-        high(14); 
-        pause(500);
-        low(14); 
-        pause(500);   
-      }    
-      //return 1;       
-    }
-    //red
-    if((r > 41) && (r < 50) && (g > 1) && (g < 7) && (b > 0) && (b < 7))  
-    {     
-      print("Red!\n"); 
-      for(int i = 0; i < 5; i++) 
-      {
-        high(14); 
-        pause(500);
-        low(14); 
-        pause(500);   
-      }
-      //return 1; 
-    }
-    //orange
-    if((r > 60) && (r < 75) && (g > 20) && (g < 40) && (b > 25) && (b < 37))  
-    {
-      print("Orange!\n"); 
-      for(int i = 0; i < 5; i++) 
-      {
-        high(14); 
-        pause(500);
-        low(14); 
-        pause(500);   
-      }  
-      //return 1;     
-    }
-    else
-    {
        print("moving straight\n");
        drive_speeds(straightSpeed_Left, straightSpeed_Right); //straight
        pause(18000); //10 [sec]
@@ -193,6 +145,5 @@ void followPattern()
   
        print("returning to original pos\n"); 
        drive_speeds(straightSpeed_Left, straightSpeed_Right); 
-       pause(18000);    
-     }        
+       pause(18000); 
 }
