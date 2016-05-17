7b9dfb3b3598a769ed2e5c01e7c890eae05673a5
Liam Heisler <liamh2017@gmail.com>
2016-05-16 23:58:48 -0400

    Update GolfPongRobot.c

--- GolfPongRobot.c
+++ GolfPongRobot.c
@@ -5,20 +5,34 @@
 #include "adcDCpropab.h"                            // Include adcDCpropAB
 #include "simpletools.h"                            // Include simpletools
 #include "fdserial.h" 
+#include "servodiffdrive.h" 
+#include "servo.h" 
+
+// CHECK ALL ANGLES
+#define liftUp_angle 1800
+#define liftDown_angle 0
+#define clawOpen_angle 10
+#define clawClose_angle 1600
+
+#define liftServo 15
+#define clawServo 14
 
 float forwardThresh 3.00
 float reverseThresh 2.00
 float rightThresh 3.00
 float leftThresh 2.00
 
-int liftUp_b
-int liftDown_b
-int clawOpen_b
-int clawClose_b
+// CHECK PINS
+int liftUp_b = 15;
+int liftDown_b = 14;
+int clawOpen_b = 13;
+int clawClose_b = 12;
 
+void handleButtonInput(void)
 int main()                                          // Main function
 {
-  fdserial *xbee; 
+  fdserial *xbee;
+  drive_pins(16, 17);  
   
   adc_init(21, 20, 19, 18);                         // CS=21, SCL=20, DO=19, DI=18
 
@@ -33,6 +47,10 @@ int main()                                          // Main function
     print("Up/Down = %.2f V %c\n", udV, CLREOL);    // Display voltage
     print("Left/Right = %.2f V %c\n", lrV, CLREOL); // Display voltage
 
+    int liftUp = input(liftUp_b);
+    int liftDown = input(liftDown_b);
+    int clawOpen = input(clawOpen_b);
+    int clawClose = input(clawClose_b);
     pause(100);                                     // Wait 1/10 s
     
     if(udV > 2.6)
@@ -71,4 +89,25 @@ int main()                                          // Main function
       low(26); 
     }
   }  
+}
+
+void handleButtonInput() 
+{
+  //Will handle all input from buttons and will also adjust pos. of lift and claw. 
+    if(liftUp == 1) 
+    {
+       servo_angle(liftServo, liftUp_angle); 
+    }
+    else if(liftDown == 1)
+    {
+       servo_angle(liftServo, liftDown_angle);
+    }
+    else if(clawOpen == 1)
+    {
+       servo_angle(clawServo, clawOpen_angle); 
+    }
+    else if(clawClose == 1)
+    {
+       servo_angle(clawServo, clawClose_angle); 
+    }
 }
