310eeeb6752d5c3168fc503a79d203c860fac3cd
Liam Heisler <liamh2017@gmail.com>
2016-05-18 09:19:51 -0400

    Update GolfPongRobot.c

--- GolfPongRobot.c
+++ GolfPongRobot.c
@@ -19,10 +19,10 @@
 #define liftServo 15
 #define clawServo 14
 
-float forwardThresh 3.00
-float reverseThresh 2.00
-float rightThresh 3.00
-float leftThresh 2.00
+float forwardThresh =3.00;
+float reverseThresh = 2.00;
+float rightThresh = 3.00;
+float leftThresh = 2.00;
 
 // CHECK PINS
 int liftUp_b = 15;
@@ -30,109 +30,90 @@ int liftDown_b = 14;
 int clawOpen_b = 13;
 int clawClose_b = 12;
 
-void handleButtonInput(void)
+fdserial *xbee;
+
+void handleButtonInput(void);
+void checkJoystick(void); 
 int main()                                          // Main function
 {
-  fdserial *xbee;
+  xbee = fdserial_open(9, 8, 0, 9600); 
+  
   drive_pins(lMotor, rMotor);  
   
-  adc_init(21, 20, 19, 18);                         // CS=21, SCL=20, DO=19, DI=18
-  float lrV, udV;                                   // Voltage variables
+  adc_init(21, 20, 19, 18);                         // CS=21, SCL=20, DO=19, DI=18                           
   
   writeChar(xbee, CLS);
-  char c;
+  char c; 
 
-  while(1)                                          // Loop repeats indefinitely
-  {
-    udV = adc_volts(2);                             // Check A/D 2                
-    lrV = adc_volts(3);                             // Check A/D 3
-   
-    putChar(HOME);                                  // Cursor -> top-left "home"
-    print("Up/Down = %.2f V %c\n", udV, CLREOL);    // Display voltage
-    print("Left/Right = %.2f V %c\n", lrV, CLREOL); // Display voltage
-
-    int liftUp = input(liftUp_b);
-    int liftDown = input(liftDown_b);
-    int clawOpen = input(clawOpen_b);
-    int clawClose = input(clawClose_b);
-    pause(100);                                     // Wait 1/10 s
+  float udV, lrV; 
+  int liftUp, liftDown, clawOpen, clawClose; 
+  
+  while(1)                                        
+  {  
+    putChar(HOME); 
     
-    // OPERATE ROBOT
-    checkJoystick();
-    if(c == 'f') 
+    udV = adc_volts(2);
+    lrV = adc_volts(3);
+    liftUp = input(liftUp_b);
+    liftDown = input(liftDown_b);
+    clawOpen = input(clawOpen_b);
+    clawClose = input(clawClose_b);
+    
+    if(udV > forwardThresh) 
     {
-      print("Forward!"); 
-      servo_speeds(90, 45);   
+      c = 'f';       
     }
-    else if(c == 'r') 
+    if(udV < reverseThresh) 
     {
-      print("Reverse!");
-      servo_speeds(-45, -90);  
+      // Reverse
+      c = 'r';   
     }
-    else if(c == 'l') 
+    if(lrV > rightThresh) 
     {
-      print("Left!");
-      servo_speeds(-5, 100);   
+      // Right 
+      c = 'r';   
     }
-    else if(c == 'r') 
+    if(lrV < leftThresh) 
     {
-      print("Right!"); 
-      servo_speeds(100, -5);  
+      // Left
+      c = 'l';     
     }
-    handleButtonInput(); 
     
     fdserial_txChar(xbee, c); 
     pause(50); 
   }  
 }
 
+/*
 void handleButtonInput() 
 {
   //Will handle all input from buttons and will also adjust pos. of lift and claw. 
+    
+    int liftUp = input(liftUp_b);
+    int liftDown = input(liftDown_b);
+    int clawOpen = input(clawOpen_b);
+    int clawClose = input(clawClose_b);
+    
+    print("LU = %d, LD = %d, CO = %d, CC = %d\n", liftUp, liftDown, clawOpen, clawClose);  
     if(liftUp == 1) 
     {
+       print("Lift Up"); 
        servo_angle(liftServo, liftUp_angle); 
     }
     else if(liftDown == 1)
     {
+       print("Lift Down");
        servo_angle(liftServo, liftDown_angle);
     }
     else if(clawOpen == 1)
     {
+       print("Claw Open"); 
        servo_angle(clawServo, clawOpen_angle); 
     }
     else if(clawClose == 1)
     {
+       print("Claw Close"); 
        servo_angle(clawServo, clawClose_angle); 
     }
 }
-void checkJoystick() 
-{
-   // GLOBAL CHAR C?
-   // CHECK IF STATEMENTS. REWRITE IF NEEDED.
-    if(udV > 2.6)
-    {
-      print("Forward!"); 
-      c = 'f';    
-    }
-    if(udV < 2.4) 
-    {
-      print("Reverse!");
-      c = 'r';    
-    }
-    if(lrV < 2.2) 
-    {
-      print("Left!");
-      c = 'l'; 
-    }
-    if(lrV > 2.6)
-    {
-      print("Right!");
-      c = 'r';  
-    }
-    else 
-    {
-       c = 's';  
-    }   
-    
-}
+*/
